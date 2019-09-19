cee0aaa8a12facd49918a9ed50803fddb40db3cd
gabivlj <gabitriqui@gmail.com>
2019-09-19 16:17:43 +0200

    CameraSM (general state machine for the game that manages the camera, we can add states etc.) and the CameraBehaviour (the real camera that actually changes and moves to the objectives, use this in CameraSM to move around the camera with a certain speed), all are done more or less, it needs more testing and actual implementation in the game.

---
+++ Assets/Code/Cameras/CameraBehaviour/CameraBehaviour.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// CameraBehaviour, call SetCamera() if you want it to move to the next objective.
+/// </summary>
+public class CameraBehaviour : MonoBehaviour
+{
+    
+    #region Private
+
+    private int id = -1;
+    private float t = 0.0f;
+    private Transform currentBehaviour = null;
+    [SerializeField]
+    private float spd = 1f;
+    private bool transitioning = false;
+    private Transform beforeBehaviour = null;
+
+    #endregion
+    
+    #region Public
+
+    #endregion
+    
+    #region Properties
+
+    public Transform CurrentBehaviour
+    {
+        get { return currentBehaviour; }
+    }
+
+    public int Id
+    {
+        get { return id; }
+    }
+
+    public float T
+    {
+        get { return t; }
+    }
+
+    public bool Transitioning
+    {
+        get { return transitioning; }
+    }
+    
+    #endregion
+    
+    #region MonoBehaviour
+    
+    void Start()
+    {
+        currentBehaviour = transform;
+    }
+
+    
+    void Update()
+    {
+        if (transitioning)
+        {
+            Transition();
+        }
+        else
+        {
+            transform.position = currentBehaviour.position;
+            transform.rotation = currentBehaviour.rotation;
+        }
+    }
+    
+    #endregion
+    
+    #region Methods
+    
+    /// <summary>
+    /// Transitioning to the current transform
+    /// </summary>
+    private void Transition()
+    {
+        // Transition finished, return
+        if (t >= 1f)
+        {
+            transitioning = false;
+            return;
+        }
+        // Update transition
+        float dt = Time.deltaTime;
+        transform.position = Vector3.Lerp(beforeBehaviour.position, currentBehaviour.position, t);
+        transform.rotation = Quaternion.Lerp(beforeBehaviour.rotation, currentBehaviour.rotation, t);
+        t += spd * dt;
+    }
+    
+    /// <summary>
+    /// Sets an objective for the camera.
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="speed"></param>
+    /// <returns></returns>
+    public bool SetCamera(GameObject go, float speed = 1f)
+    {
+        if (go.GetInstanceID() == id) 
+            return false;
+        beforeBehaviour = currentBehaviour;
+        currentBehaviour = go.transform;
+        spd = speed;
+        transitioning = true;
+        id = go.GetInstanceID();
+        t = 0.0f;
+        return true;
+    }
+    
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="speed"></param>
+    /// <returns></returns>
+    public bool SetCamera(Transform go, float speed = 1f)
+    {
+        if (go.GetInstanceID() == id)
+            return false;
+        beforeBehaviour = currentBehaviour;
+        currentBehaviour = go.transform;
+        transitioning = true;
+        spd = speed;
+        id = go.GetInstanceID();
+        t = 0.0f;
+        return true;
+    }
+    
+    #endregion
+}
---
+++ Assets/Code/Cameras/CameraBehaviour/CameraSM.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CameraManager
+{
+
+    /// <summary>
+    /// MOVE = 0, SHOOTING = 1
+    /// </summary>
+    public enum State
+    {
+        Move = 0,
+        Aiming
+    }
+    
+    public class CameraSM : MonoBehaviour
+    {
+
+        #region Public
+
+        #endregion
+
+        #region Private
+
+        [SerializeField]
+        private Transform cameraMoving;
+        [SerializeField]
+        private Transform cameraShooting;
+        private State state;
+        [SerializeField]
+        private CameraBehaviour cb;
+        
+        #endregion
+
+        #region Properties
+
+        public State State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        
+        public Transform CameraMoving
+        {
+            get { return cameraMoving; }
+            set { cameraMoving = value; }
+        }
+
+        public Transform CameraShooting
+        {
+            get { return cameraShooting; }
+            set { cameraShooting = value; }
+        }
+
+        /// <summary>
+        /// Set this in Awake() before Start of this component is executed
+        /// </summary>
+        public CameraBehaviour CBehaviour
+        {
+            get { return cb;  }
+            set { cb = value;  }
+        }
+
+        #endregion
+        
+        #region MonoBehaviour
+
+         
+        void Start()
+        {
+            Assert.IsNotNull(cb, "Please, set the CameraBehaviour before Start() of CameraSM.");
+            Assert.IsNotNull(cameraMoving, "Please, set the moving camera.");
+            Assert.IsNotNull(cameraShooting, "Please, set the shooting camera.");
+            Testing();
+        }
+    
+        void Update()
+        {
+            Assert.IsTrue(ProcessStateMachineUpdate(),
+                "Error in state machine of CameraSM.cs, see more logs to see what is wrong.");
+        }
+
+        #endregion
+        
+        #region Methods
+
+        private bool ProcessStateMachineUpdate()
+        {
+            if (!cb)
+            {
+                return false;
+            }
+            switch (state)
+            {
+                case State.Move:
+                    cb.SetCamera(cameraMoving, 1.0f);
+                    break;
+                case State.Aiming:
+                    cb.SetCamera(cameraShooting, 1.5f);
+                    break;
+                default:
+                    Assert.IsTrue(false, "Unknown state.");
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void Testing()
+        {
+          StartCoroutine(ChangeStateTest());
+        }
+        
+        IEnumerator ChangeStateTest()
+        {
+            yield return new WaitForSeconds(3f);
+            if (state == State.Aiming) state = State.Move;
+            else state = State.Aiming;
+            StartCoroutine(ChangeStateTest());
+        }
+        
+        #endregion
+
+        #region Properties
+
+        
+
+        #endregion
+
+        
+       
+    }   
+
+}
