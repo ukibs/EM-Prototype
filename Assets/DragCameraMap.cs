3f21c24a166035f3acfd2bc8524855aaffc69824
eltochetao <erni.uson@gmail.com>
2019-10-25 10:04:58 +0200

    Martin explicame los scripts de los botones del mapa

---
+++ Assets/DragCameraMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragCameraMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(-(pos.x * dragSpeed), pos.y * dragSpeed, 0);
+
+            transform.Translate(move, Space.World);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+}
